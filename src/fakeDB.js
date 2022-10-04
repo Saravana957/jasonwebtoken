b1c3b922b80f43b6a4fdec5f364b19c9034b4c3b
Saravanakumar Murugesan/Ads Product Management /NA Service Biz/Engineer/Samsung Electronics <s.murugesan@sdsa002774.na.corp.samsungelectronics.net>
2022-10-03 23:20:16 -0500

    register user

---
+++ src/fakeDB.js
@@ -0,0 +1,10 @@
+exports.fakeDB=[];
+
+
+// exports.fakeDB = [
+//     {
+//       id: 0,
+//       email: 'thomas.weibenfalk@gmail.com',
+//       password: '$2a$12$x5S4Bapkrv5YXnghtMqUjusW/lucAJLkO3.IJaajGtBnVrwXQ06u.', // Password: test
+//     },
+//   ];
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
+const { fakeDB } = require('./fakeDB.js');
 
 //1. Register a user
 //2. Login a user
@@ -14,7 +15,7 @@ const { hash, compare } = require('bcryptjs');
 const server = express();
 
 //Use express middleware for easier cookie handling
-server.use(cookieParser);
+server.use(cookieParser());
 
 server.use(
     cors({
@@ -26,6 +27,30 @@ server.use(
 //needed to be able to read json body data
 server.use(express.json());//to support json encoded bodies. 
 server.use(express.urlencoded({extended: true})); //support url encoded bodies. 
+
+//1. Register a user
+server.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        // 1. check if the user exist
+        const user = fakeDB.find(user => user.email === email)
+        if (user) throw new Error('User already exist');
+        const hashedPassword = await hash(password, 10);
+        fakeDB.push({
+            id: fakeDB.length,
+            email,
+            password: hashedPassword,
+        })
+        res.send({message: 'User Created'})
+        console.log(fakeDB); 
+    } catch(err){
+        res.send({
+            error: `${err.message}`,
+        })
+    }
+})
+
 
 server.listen(process.env.PORT, () => {
     console.log(`server listening on port ${process.env.PORT}`);
